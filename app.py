4c8585433cd0de5b858f334a1e49129a606765d1
Org877 <oreyesg747@gmail.com>
2024-10-30 15:43:49 -0600

    Ultimos cambios

--- app.py
+++ app.py
@@ -6,11 +6,33 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/saludos')
-def saludos():
-    return "Que pasa bro"
+@app.route('/Mipaginaweb.html')
+def mipaginaweb():
+    return render_template('Mipaginaweb.html')
 
+@app.route('/Git.html')
+def git():
+    return render_template('Git.html')
+
+@app.route('/VR.html')
+def vr():
+    return render_template('VR.html')
+
+@app.route('/escaneo3d.html')
+def escaneo3d():
+    return render_template('escaneo3d.html')
+
+@app.route('/Investigacion.html')
+def Investigacion():
+    return render_template('Investigacion.html')
+
+@app.route('/Juegos.html')
+def Juegos():
+    return render_template('Juegos.html')
+
+@app.route('/Presentacion.html')
+def Presentacion():
+    return render_template('Presentacion.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
